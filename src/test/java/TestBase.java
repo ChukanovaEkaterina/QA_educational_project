71159705ecab7fa50cffaba805c39aea202d45df
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-05-29 21:25:48 +0300

    Edited TestBase

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,10 +1,21 @@
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-public class TestBase {    @BeforeAll
-static void beforeAll(){
-    Configuration.browserSize = "1920x1088";
-    Configuration.browser = "chrome";
-    Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1280";
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @AfterEach
+    void afterEach () {
+        closeWebDriver();
+    }
 }
-}
+
