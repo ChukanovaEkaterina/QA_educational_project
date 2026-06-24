b61b50d1a07ac8515624a6b4cc5939a499dfca9d
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-06-24 14:24:14 +0300

    add annotations junit5

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -7,22 +11,26 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationTests extends TestBase {
 
-    @Test
-    void successfulRegistrationTest() {
+    @CsvSource (value = {
+            "Female, Sports",
+            "Male, Reading",
+            "Other, Music"
+    })
+    @ParameterizedTest(name = "Успешная регистрация с полом: {0} и хобби: {1} при вводе всех полей валидными данными")
+    void successfulRegistrationTest(String genterWrapper, String hobbiesWrapper) {
         open("/automation-practice-form");
 
         $("#firstName").setValue("Katya");
         $("#lastName").setValue("Chukanova");
         $("#userEmail").setValue("Chukanova@mail.ru");
-        $("#genterWrapper").$(byText("Female")).click();
+        $("#genterWrapper").$(byText(genterWrapper)).click();
         $("#userNumber").setValue("1234512345");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("June");
         $(".react-datepicker__year-select").selectOption("1997");
         $(".react-datepicker__day--001").click();
         $("#subjectsInput").setValue("Art").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText(hobbiesWrapper)).click();
         $("#uploadPicture").uploadFromClasspath("photo.jpg");
         $("#currentAddress").setValue("Mira Street, 15");
         $("#react-select-3-input").setValue("RAJ").pressEnter();
@@ -30,19 +38,21 @@ public class RegistrationTests extends TestBase {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-hover").shouldHave(text("Katya Chukanova"));
-        $(".table-hover").shouldHave(text("Chukanova@mail.ru"));
-        $(".table-hover").shouldHave(text("Female"));
-        $(".table-hover").shouldHave(text("1234512345"));
-        $(".table-hover").shouldHave(text("01 June,1997"));
-        $(".table-hover").shouldHave(text("Arts"));
-        $(".table-hover").shouldHave(text("Sports, Music"));
-        $(".table-hover").shouldHave(text("photo.jpg"));
-        $(".table-hover").shouldHave(text("Mira Street, 15"));
-        $(".table-hover").shouldHave(text("Rajasthan"));
-        $(".table-hover").shouldHave(text("Jaipur"));
+        $(".table-responsive").shouldHave(text("Katya Chukanova"));
+        $(".table-responsive").shouldHave(text("Chukanova@mail.ru"));
+        $(".table-responsive").shouldHave(text(genterWrapper));
+        $(".table-responsive").shouldHave(text("1234512345"));
+        $(".table-responsive").shouldHave(text("01 June,1997"));
+        $(".table-responsive").shouldHave(text("Arts"));
+        $(".table-responsive").shouldHave(text(hobbiesWrapper));
+        $(".table-responsive").shouldHave(text("photo.jpg"));
+        $(".table-responsive").shouldHave(text("Mira Street, 15"));
+        $(".table-responsive").shouldHave(text("Rajasthan"));
+        $(".table-responsive").shouldHave(text("Jaipur"));
     }
 
+    @Tag("Regression")
+    @DisplayName("Успешная регистрация при вводе обязательных полей валидными данными")
     @Test
     void requiredFieldsRegistrationTest() {
         open("/automation-practice-form");
@@ -59,6 +69,8 @@ public class RegistrationTests extends TestBase {
         $(".table-hover").shouldHave(text("1234512345"));
     }
 
+    @Tag("Regression")
+    @DisplayName("Отсутствует обязательное поле ввода email")
     @Test
     void invalidEmailTest() {
         open("/automation-practice-form");
@@ -74,6 +86,8 @@ public class RegistrationTests extends TestBase {
         $(".table-hover").shouldNotBe(visible);
     }
 
+    @Tag("Regression")
+    @DisplayName("Невалидное поле ввода userNumber")
     @Test
     void invalidShortUserNumberTest() {
         open("/automation-practice-form");
@@ -87,7 +101,8 @@ public class RegistrationTests extends TestBase {
         $("#userNumber").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         $(".table-hover").shouldNotBe(visible);
     }
-
+    @Tag("Regression")
+    @DisplayName("Отсутствует обязательное поле ввода firstName")
     @Test
     void noRequiredfieldFirstNameTest() {
         open("/automation-practice-form");
@@ -101,6 +116,8 @@ public class RegistrationTests extends TestBase {
         $(".table-hover").shouldNotBe(visible);
     }
 
+    @Tag("Regression")
+    @DisplayName("Отправка пустой формы регистрации")
     @Test
     void allInputFieldsEmptyTest() {
         open("/automation-practice-form");
