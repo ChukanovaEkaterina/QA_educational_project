00b2ee8027853f98eeb7e16321580a78856f5c68
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-09-04 23:31:59 +0300

    Add steps and attachment

--- src/test/java/test/RegistrationTests.java
+++ src/test/java/test/RegistrationTests.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import testdata.TestData;
 
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
 
 public class RegistrationTests extends TestBase {
 
@@ -17,72 +19,94 @@ public class RegistrationTests extends TestBase {
     @DisplayName("Успешная регистрация при вводе всех полей валидными данными")
     void successfulRegistrationTest() {
 
-        TestData testData = new TestData();
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
 
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .typeFirstName(testData.firstName)
-                .typeLastName(testData.lastName)
-                .typeUserEmail(testData.userEmail)
-                .setGender(testData.genderWrapper)
-                .typeUserNumber(testData.userNumber)
-                .setDateOfBirth(testData.BirthDay, testData.BirthMonth, testData.BirthYear)
-                .setSubjects(testData.subjects)
-                .setHobbies(testData.hobbies)
-                .pictureUpload(testData.picture)
-                .typeCurrentAddress(testData.currentAddress)
-                .setStateAndCity(testData.state, testData.city)
-                .submit()
+        step("Fill registration form", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .typeFirstName(testData.firstName)
+                    .typeLastName(testData.lastName)
+                    .typeUserEmail(testData.userEmail)
+                    .setGender(testData.genderWrapper)
+                    .typeUserNumber(testData.userNumber)
+                    .setDateOfBirth(testData.BirthDay, testData.BirthMonth, testData.BirthYear)
+                    .setSubjects(testData.subjects)
+                    .setHobbies(testData.hobbies)
+                    .pictureUpload(testData.picture)
+                    .typeCurrentAddress(testData.currentAddress)
+                    .setStateAndCity(testData.state, testData.city)
+                    .submit();
+        });
 
-                .chekFormModal(testData.textSuccessfulRegistrationForm)
-                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResult("Student Email", testData.userEmail)
-                .checkResult("Gender", testData.genderWrapper)
-                .checkResult("Mobile", testData.userNumber)
-                .checkResult("Date of Birth", testData.BirthDay + " " + testData.BirthMonth + "," + testData.BirthYear)
-                .checkResult("Subjects", testData.subjects)
-                .checkResult("Hobbies", testData.hobbies)
-                .checkResult("Picture", testData.picture)
-                .checkResult("Address", testData.currentAddress)
-                .checkResult("State and City", testData.state + " " + testData.city);
+        step("Check registration form results data", () -> {
+            registrationTestPage
+                    .chekFormModal(testData.textSuccessfulRegistrationForm)
+                    .checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResult("Student Email", testData.userEmail)
+                    .checkResult("Gender", testData.genderWrapper)
+                    .checkResult("Mobile", testData.userNumber)
+                    .checkResult("Date of Birth", testData.BirthDay + " " + testData.BirthMonth + "," + testData.BirthYear)
+                    .checkResult("Subjects", testData.subjects)
+                    .checkResult("Hobbies", testData.hobbies)
+                    .checkResult("Picture", testData.picture)
+                    .checkResult("Address", testData.currentAddress)
+                    .checkResult("State and City", testData.state + " " + testData.city);
+        });
     }
 
     @Tag("Regression")
     @DisplayName("Успешная регистрация при вводе обязательных полей валидными данными")
     @Test
     void requiredFieldsRegistrationTest() {
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .typeFirstName(testData.firstName)
-                .typeLastName(testData.lastName)
-                .setGender(testData.genderWrapper)
-                .typeUserNumber(testData.userNumber)
-                .submit()
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
 
-                .chekFormModal(testData.textSuccessfulRegistrationForm)
-                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResult("Gender", testData.genderWrapper)
-                .checkResult("Mobile", testData.userNumber);
+        step("Fill registration form", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .typeFirstName(testData.firstName)
+                    .typeLastName(testData.lastName)
+                    .setGender(testData.genderWrapper)
+                    .typeUserNumber(testData.userNumber)
+                    .submit();
+        });
+
+        step("Check registration form results data", () -> {
+            registrationTestPage
+                    .chekFormModal(testData.textSuccessfulRegistrationForm)
+                    .checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResult("Gender", testData.genderWrapper)
+                    .checkResult("Mobile", testData.userNumber);
+        });
     }
 
     @Tag("Regression")
     @DisplayName("Невалидные данные в  поле ввода email")
     @Test
     void invalidEmailTest() {
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .typeFirstName(testData.firstName)
-                .typeLastName(testData.lastName)
-                .typeUserEmail(testData.lastName)
-                .setGender(testData.genderWrapper)
-                .typeUserNumber(testData.userNumber)
-                .submit()
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
 
-                .checkErrorUserEmail()
-                .notVisibleTableHover();
+        step("Fill registration data with invalid Email", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .typeFirstName(testData.firstName)
+                    .typeLastName(testData.lastName)
+                    .typeUserEmail(testData.lastName)
+                    .setGender(testData.genderWrapper)
+                    .typeUserNumber(testData.userNumber)
+                    .submit();
+        });
+
+        step("Check registration results data", () -> {
+            registrationTestPage
+                    .checkErrorUserEmail()
+                    .notVisibleTableHover();
+        });
     }
 
     @Tag("Regression")
@@ -91,48 +115,72 @@ public class RegistrationTests extends TestBase {
             "null", "0", "bobobo", "111"
     })
     void invalidShortUserNumberTest(String invalidNumber) {
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .typeFirstName(testData.firstName)
-                .typeLastName(testData.lastName)
-                .setGender(testData.genderWrapper)
-                .typeUserNumber(invalidNumber)
-                .submit()
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
 
-                .checkErrorUserNumber()
-                .notVisibleTableHover();
+        step("Fill registration data with invalid userNumber", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .typeFirstName(testData.firstName)
+                    .typeLastName(testData.lastName)
+                    .setGender(testData.genderWrapper)
+                    .typeUserNumber(invalidNumber)
+                    .submit();
+        });
+
+        step("Check registration results data", () -> {
+            registrationTestPage
+                    .checkErrorUserNumber()
+                    .notVisibleTableHover();
+        });
     }
 
     @Tag("Regression")
     @DisplayName("Отсутствует обязательное поле ввода firstName")
     @Test
     void noRequiredfieldFirstNameTest() {
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .typeLastName(testData.lastName)
-                .setGender(testData.genderWrapper)
-                .typeUserNumber(testData.userNumber)
-                .submit()
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
 
-                .checkErrorFirstName()
-                .notVisibleTableHover();
+        step("Fill registration data without invalid firstName", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .typeLastName(testData.lastName)
+                    .setGender(testData.genderWrapper)
+                    .typeUserNumber(testData.userNumber)
+                    .submit();
+        });
+
+        step("Check registration results data", () -> {
+            registrationTestPage
+                    .checkErrorFirstName()
+                    .notVisibleTableHover();
+        });
     }
 
     @Tag("Regression")
     @DisplayName("Отправка пустой формы регистрации")
     @Test
     void allInputFieldsEmptyTest() {
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .submit()
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
 
-                .checkErrorFirstName()
-                .checkErrorLastName()
-                .checkErrorUserNumber()
-                .notVisibleTableHover();
+        step("Fill registration data with all input fields empty", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .submit();
+        });
+
+        step("Check registration results data", () -> {
+            registrationTestPage
+                    .checkErrorFirstName()
+                    .checkErrorLastName()
+                    .checkErrorUserNumber()
+                    .notVisibleTableHover();
+        });
     }
 
     @Tag("Regression")
@@ -143,19 +191,28 @@ public class RegistrationTests extends TestBase {
 
     void linkingUserNumberFirstNameLastName(String firstName, String lastName,
                                             String phoneNumber, String firstAndLastName) {
-        registrationTestPage
-                .openPage()
-                .scrollHeight()
-                .typeFirstName(firstName)
-                .typeLastName(lastName)
-                .setGender(testData.genderWrapper)
-                .typeUserNumber(phoneNumber)
-                .submit()
 
-                .chekFormModal(testData.textSuccessfulRegistrationForm)
-                .checkResult("Student Name", firstAndLastName)
-                .checkResult("Gender", testData.genderWrapper)
-                .checkResult("Mobile", phoneNumber);
+        step("Open Student Registration page", () -> {
+            registrationTestPage.openPage();
+        });
+
+        step("Fill registration data with link Number, FirstName and LastName", () -> {
+            registrationTestPage
+                    .scrollHeight()
+                    .typeFirstName(firstName)
+                    .typeLastName(lastName)
+                    .setGender(testData.genderWrapper)
+                    .typeUserNumber(phoneNumber)
+                    .submit();
+        });
+
+        step("Check registration results data", () -> {
+            registrationTestPage
+                    .chekFormModal(testData.textSuccessfulRegistrationForm)
+                    .checkResult("Student Name", firstAndLastName)
+                    .checkResult("Gender", testData.genderWrapper)
+                    .checkResult("Mobile", phoneNumber);
+        });
     }
 }
 
--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,9 +1,17 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationTestPage;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -11,14 +19,31 @@ public class TestBase {
 
     RegistrationTestPage registrationTestPage = new RegistrationTestPage();
 
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
     @BeforeAll
-    static void setupEnvironment(){
+    static void setupEnvironment() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.qa.guru/wd/hub";
     }
 
     @AfterEach
-    void tearDown() {
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
         closeWebDriver();
     }
 }
