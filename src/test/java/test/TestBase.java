04d937eb447037b04ffd096c26039a94d8ecd8e8
ChukanovaEkaterina <katerinaluzhnykh25@mail.ru>
2026-09-07 15:10:36 +0300

    add environment parameterization

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -7,7 +7,6 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.RegistrationTestPage;
 
@@ -26,16 +25,23 @@ public class TestBase {
 
     @BeforeAll
     static void setupEnvironment() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize = System.getProperty( "browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.qa.guru/wd/hub";
+
+        String remoteUrl = System.getProperty("remoteUrl");
+        if (remoteUrl == null || remoteUrl.isEmpty() || "null".equals(remoteUrl)) {
+            remoteUrl = "https://user1:1234@selenoid.qa.guru/wd/hub";
+        }
+        Configuration.remote = remoteUrl;
     }
 
     @AfterEach
